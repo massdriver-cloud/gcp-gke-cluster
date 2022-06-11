b9c8f2e246ef19ea0f5aa91ab1f01bba770b78b2
Will <will@containerlabs.io>
2022-06-11 11:29:51 -0700

    remove local modules, use remote

--- src/apis.tf
+++ src/apis.tf
@@ -1,4 +1,4 @@
 module "apis" {
-  source   = "github.com/massdriver-cloud/terraform-google-enable-apis"
+  source   = "github.com/massdriver-cloud/terraform-modules//gcp-enable-apis?ref=c336d59"
   services = ["iam.googleapis.com", "container.googleapis.com"]
 }
