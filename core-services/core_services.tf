0c08b53bb9ccb5081479716aec957fdcb26c62c1
Will <will@containerlabs.io>
2022-08-26 10:15:36 -0700

    pull in new module, use const / title for machine types

--- core-services/core_services.tf
+++ core-services/core_services.tf
@@ -53,7 +53,7 @@ module "ingress_nginx" {
 }
 
 module "external_dns" {
-  source                  = "github.com/massdriver-cloud/terraform-modules//k8s-external-dns-gcp?ref=31f17d3"
+  source                  = "github.com/massdriver-cloud/terraform-modules//k8s-external-dns-gcp?ref=2af53ce"
   count                   = local.enable_external_dns ? 1 : 0
   kubernetes_cluster      = local.kubernetes_cluster_artifact
   md_metadata             = var.md_metadata
