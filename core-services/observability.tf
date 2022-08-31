3f72c7078d87b6f88c0fbc6e995f6989a8f17d63
Jake Ferriero <jtferriero@gmail.com>
2022-08-31 13:49:02 -0700

    add fluentbit to  opensearch log shipping (#19)
    * add opensearch option
    
    * hard code enable dashboards true
    
    * wip
    
    * commit ref or opensearch module
    
    * wip: clean apply dns issue
    
    * opensearch SSL issue
    
    * trying to fix password
    
    * opensearch and fluentbit working without tls and using admin:admin user
    
    * update massdriver.yaml
    
    * remove dev reset script
    
    * fixup pre-commit
    
    * remove anything related to internal users
    
    * rm reset.sh

--- core-services/observability.tf
+++ core-services/observability.tf
@@ -1,8 +1,50 @@
+locals {
+  enable_opensearch = var.observability.logging.destination == "opensearch"
+  enable_fluentbit  = var.observability.logging.collection == "fluentbit"
+  o11y_namespace    = "md-observability"
+}
+
 // Unless the user is running prometheus (or integrates an observability package like DD)
 // there isn't much point to this service.
 module "kube-state-metrics" {
   source      = "github.com/massdriver-cloud/terraform-modules//k8s-kube-state-metrics?ref=c336d59"
   md_metadata = var.md_metadata
   release     = "kube-state-metrics"
-  namespace   = "md-observability"
+  namespace   = local.o11y_namespace
+}
+
+module "opensearch" {
+  count              = local.enable_opensearch ? 1 : 0
+  source             = "github.com/massdriver-cloud/terraform-modules//k8s-opensearch?ref=5fc9525"
+  md_metadata        = var.md_metadata
+  release            = "opensearch"
+  namespace          = local.o11y_namespace
+  kubernetes_cluster = local.kubernetes_cluster_artifact
+  helm_additional_values = {
+    persistence = {
+      size = var.observability.logging.opensearch.persistence_size
+    }
+  }
+  enable_dashboards = true
+  // this adds a retention policy to move indexes to warm after 1 day and delete them after a user configurable number of days
+  ism_policies = {
+    "hot-warm-delete" : templatefile("${path.module}/logging/opensearch/ism_hot_warm_delete.json.tftpl", { "log_retention_days" : var.observability.logging.opensearch.retention_days })
+  }
+}
+
+module "fluentbit" {
+  count = local.enable_fluentbit ? 1 : 0
+  # TODO replace ref with a SHA once k8s-fluentbit is merged
+  source             = "github.com/massdriver-cloud/terraform-modules//k8s-fluentbit?ref=k8s-fluentbit"
+  md_metadata        = var.md_metadata
+  release            = "fluentbit"
+  namespace          = local.o11y_namespace
+  kubernetes_cluster = local.kubernetes_cluster_artifact
+  helm_additional_values = {
+    config = {
+      outputs = templatefile("${path.module}/logging/fluentbit/opensearch_output.conf.tftpl", {
+        namespace = local.o11y_namespace
+      })
+    }
+  }
 }
