d8a668266ad463fb392be85e091b1f554b2bcf76
Will <will@containerlabs.io>
2022-12-20 18:33:02 -0800

    cleanup

--- core-services/_providers.tf
+++ core-services/_providers.tf
@@ -37,6 +37,14 @@ data "google_container_cluster" "cluster" {
   location = var.subnetwork.specs.gcp.region
 }
 
+locals {
+  cluster_ca_certificate = base64decode(data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+  cluster_token          = data.google_client_config.provider.access_token
+  cluster_host           = "https://${data.google_container_cluster.cluster.endpoint}"
+  cluster_name           = var.md_metadata.name_prefix
+  cluster_network_tag    = "gke-${local.cluster_name}"
+}
+
 provider "google" {
   project     = var.gcp_authentication.data.project_id
   credentials = jsonencode(var.gcp_authentication.data)
--- core-services/main.tf
+++ core-services/main.tf
@@ -1,3 +1,16 @@
 module "gcp-gke-core-services" {
   source = "../../terraform-modules/gcp-gke-core-services"
+  md_metadata = var.md_metadata
+  logging = {
+    opensearch = {
+      enabled = var.observability.logging.destination == "opensearch"
+      persistence_size_gi = try(var.observability.logging.opensearch.persistence_size, 10)
+      retention_days = try(var.observability.logging.opensearch.retention_days, 7)
+    }
+  }
+  vpc_grn = var.subnetwork.data.infrastructure.gcp_global_network_grn
+  gcp_project_id = var.gcp_authentication.data.project_id
+  gcp_region = var.subnetwork.specs.gcp.region
+  cloud_dns_managed_zones = var.core_services.cloud_dns_managed_zones
+  control_plane_ipv4_cidr_block = var.cluster_networking.master_ipv4_cidr_block
 }
