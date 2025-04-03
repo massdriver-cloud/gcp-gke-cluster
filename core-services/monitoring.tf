6a937156efa7262ef862a7830e057ea05c3edd7c
chrisghill <chris@massdriver.cloud>
2025-04-03 13:27:46 -0600

    Update the module ref of ingress to use 4.12.1 for CVE

--- core-services/monitoring.tf
+++ core-services/monitoring.tf
@@ -1,6 +1,6 @@
 module "alarm_channel" {
   count       = true ? 1 : 0
-  source      = "github.com/massdriver-cloud/terraform-modules//k8s/alarm-channel?ref=41e799c"
+  source      = "github.com/massdriver-cloud/terraform-modules//k8s/alarm-channel?ref=42d293b"
   md_metadata = var.md_metadata
   namespace   = kubernetes_namespace_v1.md-observability.metadata.0.name
   release     = var.md_metadata.name_prefix
@@ -10,7 +10,7 @@ module "alarm_channel" {
 
 module "application_alarms" {
   count             = true ? 1 : 0
-  source            = "github.com/massdriver-cloud/terraform-modules//massdriver/k8s-application-alarms?ref=41e799c"
+  source            = "github.com/massdriver-cloud/terraform-modules//massdriver/k8s-application-alarms?ref=42d293b"
   md_metadata       = var.md_metadata
   pod_alarms        = true
   deployment_alarms = true
